36bb10217284b34bca8eab994c2f3689231ece8b
Zerkay <42772319+ZerkayDev@users.noreply.github.com>
2018-10-07 02:21:38 +0200

    Update AutoStartRichPresence.plugin.js

--- plugins/AutoStartRichPresence.plugin.js
+++ plugins/AutoStartRichPresence.plugin.js
@@ -1,32 +1,10 @@
-//META{"name":"AutoStartRichPresence","website":"https://github.com/Mega-Mewthree/BetterDiscordPlugins/tree/master/Plugins/AutoStartRichPresence","source":"https://github.com/Mega-Mewthree/BetterDiscordPlugins/blob/master/Plugins/AutoStartRichPresence/AutoStartRichPresence.plugin.js"}*//
-
-/*
------BEGIN PGP SIGNED MESSAGE-----
-Hash: SHA256
+//META{"name":"ZerkayCustomGame","Autor":"Zerkay","source":"https://github.com/ZerkayDev/ZerkayFont/edit/master/plugins/AutoStartRichPresence.plugin.js"}*//
 
 */
 /*
-MIT License
 
-Copyright (c) 2018 Mega_Mewthree
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+Copyright (c) 2018 Zerkay
+(RP)
 */
 
 // Updated July 23rd, 2018.
